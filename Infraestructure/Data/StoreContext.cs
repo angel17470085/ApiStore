88f036dee6b2722fb20003d76765a9a91d5b0b95
angel17470085 <17470085@campeche.tecnm.mx>
2022-09-23 00:15:23 -0500

    Initial Commit

---
+++ Infraestructure/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infraestructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext (DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+
+
+    }
+}
